8cbcc2848c721c4a4368f695d5d633606ab88b98
Brooke McFarland <brookelyn.mcf@gmail.com>
2016-07-02 14:10:00 -0500

    Initial Commit

---
+++ GraysPortableWelding/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using GraysPortableWelding.Models;
+using GraysPortableWelding.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GraysPortableWelding.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            ServicesViewModel servicesViewModel = new ServicesViewModel();
+            servicesViewModel.Services = new List<Service>();
+            Service service1 = new Service() { Description = "welding using a tungstene rod", Name = "tig", Price = 450.50 };
+            Service service2 = new Service() { Description = "welding using a wire electrode", Name = "mig", Price = 550.50 };
+            Service service3 = new Service() { Description = "welding using a stick", Name = "stick welding", Price = 650.50 };
+
+            servicesViewModel.Services.Add(service1);
+            servicesViewModel.Services.Add(service2);
+            servicesViewModel.Services.Add(service3);
+
+
+            return View(servicesViewModel);
+        }
+
+    }
+}
---
+++ GraysPortableWelding/ViewModels/ServicesViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GraysPortableWelding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraysPortableWelding.ViewModels
+{
+    public class ServicesViewModel
+    {
+        public List<Service> Services { get; set; }
+
+        public double TotalPrice
+        {
+            get
+            {
+                double total = 0;
+                foreach (Service service in this.Services)
+                {
+                    total += service.Price;
+                }
+                return total;
+            }
+        }
+    }
+}
